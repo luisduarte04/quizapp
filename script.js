a625383181c724c5bd6e9f6664fd789b731ecc71
Luis Guilherme <lgduarte.santos2@hotmail.com>
2024-06-17 02:30:12 +0000

    ajustes da função de selcionar assunto para funcionar em todos os dispositivos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 const botaoTema = document.querySelector(".tema button")
+
 const body = document.querySelector("body")
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
